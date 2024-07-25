7261de9b97e0cb31991c784f780f5a741cd0779e
Janek <eunho.moon@gmail.com>
2024-07-25 22:59:17 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const pixelArtData = [
+  ['white', 'white', 'white', 'white', 'black', 'black', 'white', 'white', 'white', 'white'],
+  ['white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white'],
+  ['white', 'white', 'black', 'white', 'white', 'white', 'white', 'black', 'white', 'white'],
+  ['white', 'white', 'black', 'white', 'white', 'white', 'white', 'black', 'white', 'white'],
+  ['white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white'],
+  ['white', 'black', 'white', 'white', 'white', 'white', 'white', 'white', 'black', 'white'],
+  ['white', 'black', 'white', 'white', 'black', 'black', 'white', 'white', 'black', 'white'],
+  ['white', 'white', 'black', 'white', 'white', 'white', 'white', 'black', 'white', 'white'],
+  ['white', 'white', 'white', 'black', 'white', 'white', 'black', 'white', 'white', 'white'],
+  ['white', 'black', 'black', 'white', 'black', 'black', 'white', 'black', 'black', 'white'],
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pixel-art">
+        {pixelArtData.flat().map((color, index) => (
+          <div key={index} className={`pixel ${color}`}></div>
+        ))}
+      </div>
     </div>
   );
 }
